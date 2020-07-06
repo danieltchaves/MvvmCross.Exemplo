14ec021988f293711f33536389f13c96422ed64c
Daniel Chaves <danielTchaves@hotmail.com>
2020-07-05 21:07:51 -0300

    ok

--- MvvmCross.Exemplo/MvvmCross.Exemplo.Android/MainActivity.cs
+++ MvvmCross.Exemplo/MvvmCross.Exemplo.Android/MainActivity.cs
@@ -19,7 +19,7 @@ namespace MvvmCross.Exemplo.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            //LoadApplication(new CoreApp());
+            //LoadApplication(new App());
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
